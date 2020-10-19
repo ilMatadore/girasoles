1e872daf24d76364c3d485dfb9eb4db6cfd67aeb
ilMatadore <restarter.dev@gmail.com>
2020-10-19 11:53:47 -0500

    removing console errors

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -19,7 +19,7 @@ import { useHistory } from "react-router-dom";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
+
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
